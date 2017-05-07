629ea2215e8f34e6a24ed3446aafb03d5ff44694
Ricardo Jordao <aluno18451@ipt.pt>
2017-05-08 00:51:02 +0100

    codigo com o construtor da classe Empregados

--- POS_aplicacao/POS_aplicacao/Models/Empregados.cs
+++ POS_aplicacao/POS_aplicacao/Models/Empregados.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace POS_aplicacao.Models
 {
+    // vai representar os dados da tabela dos EMPREGADOS
+
+    // criar o construtor desta classe
     public class Empregados
     {
+        [Key]//indica que o atributo e uma PK
+        [Display(Name = "Cliente ID")]
+        public int ClienteID { get; set; }
+
+        [Display(Name = "Nome do Dono")]
+        public string Nome { set; get; }
+
+        [Display(Name = "N. Contribuinte")]
+        public string NIF { get; set; }
+
+        [Display(Name = "Telefone")]
+        public string Telefone { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Morada")]
+        public string Morada { get; set; }
     }
 }
