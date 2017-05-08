743a9a48d8cd98d68ee6fc0cd80754d58777c5b9
Ricardo Jordao <aluno18451@ipt.pt>
2017-05-08 18:46:44 +0100

    enable migrations

---
+++ POS_aplicacao/POS_aplicacao/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace POS_aplicacao.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<POS_aplicacao.Models.PosAppDB>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "POS_aplicacao.Models.PosAppDB";
+        }
+
+        protected override void Seed(POS_aplicacao.Models.PosAppDB context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
