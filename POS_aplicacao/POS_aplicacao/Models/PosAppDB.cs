52d759fe57ebdc7b671809538e0351301cda6936
Ricardo Jordao <aluno18451@ipt.pt>
2017-05-08 03:44:56 +0100

    descrisao das tabelas

--- POS_aplicacao/POS_aplicacao/Models/PosAppDB.cs
+++ POS_aplicacao/POS_aplicacao/Models/PosAppDB.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace POS_aplicacao.Models
 {
-       //inserir comentario
-    public class PosAppDB
+    public class PosAppDB : DbContext
     {
-        //jhjhjhj
+        //representar a database
+        //descrevendo as tabelas que la estao contidas
+
+        //representar um construtor desta classe
+        //identifica onde se encontra a database
+
+        public PosAppDB() : base("POSappDBConnection") { }
+        //descrever as tabelas que estao na database
+        public virtual DbSet<Clientes> Clientes { get; set; }
+        public virtual DbSet<Empregados> Empregados { get; set; }
+        public virtual DbSet<Fornecedores> Fornecedores { get; set; }
+        public virtual DbSet<Produtos> Produtos { get; set; }
+        public virtual DbSet<Vendas> Vendas { get; set; }
     }
 }
