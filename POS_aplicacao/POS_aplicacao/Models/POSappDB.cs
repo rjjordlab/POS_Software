f4b1337b3706ece3885f0a2c4a88c657a900b8d7
Ricardo Jordao <aluno18451@ipt.pt>
2017-05-08 03:23:00 +0100

    Configurar a representacao da DB

--- POS_aplicacao/POS_aplicacao/Models/POSappDB.cs
+++ POS_aplicacao/POS_aplicacao/Models/POSappDB.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace POS_aplicacao.Models
 {
-       //inserir comentario
-    public class PosAppDB
+    public class PosAppDB : DbContext
     {
-        //jhjhjhj
+        //representar a database
+        //descrevendo as tabelas que la estao contidas
+
+        //representar um construtor desta classe
+        //identifica onde se encontra a database
+
+        public PosAppDB() : base("POSappDBConnection") { }
+
+        //descrever as tabelas que estao na database
+     //   public virtual DbSet<Donos> Donos { get; set; }
+      //  public virtual DbSet<Animais> Animais { get; set; }
+      //  public virtual DbSet<Veterinarios> Veterinarios { get; set; }
+       // public virtual DbSet<Consultas> Consultas { get; set; }
     }
 }
